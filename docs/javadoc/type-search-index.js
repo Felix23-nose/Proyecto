f65dacb12a8d3e58a5323d75f2153bda2345d0a6
Felix Puma <felixpuma810@gmail.com>
2026-05-25 18:01:56 +0200

    docs: generate javadoc documentation

---
+++ docs/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.ecoroute.model","l":"Contenedor"},{"p":"com.ecoroute.model","l":"Operario"},{"p":"com.ecoroute.model","l":"Recogida"},{"p":"com.ecoroute.model","l":"Ruta"},{"p":"com.ecoroute.model","l":"Vehiculo"}];updateSearchResults();
